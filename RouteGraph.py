b1b0ecc3dd20425bbcd16d6aa16508593fb9f5f8
pavmin <eminpav@gmail.com>
2019-01-20 00:50:49 +0300

    implemented Bellman-Ford algorith in get_short_route() method, still need to find shortest route

--- RouteGraph.py
+++ RouteGraph.py
@@ -13,26 +13,39 @@ class RouteGraph:
     def __init__(self, start, end, refill=()):  # refill - set of points (x,y), start(end) - set (x,y)
         if len(refill):
             self.coordinates = np.vstack([np.array(start),
-                                      np.vstack(refill),
-                                      np.array(end)])
+                                          np.vstack(refill),
+                                          np.array(end)])
         else:
             self.coordinates = np.vstack([np.array(start),
                                           np.array(end)])
         self.adjacency = adj_matrix_comp(self.coordinates)
 
-    def get_edges_length(self):
+    def get_edges_weight(self):  # weights are equal to length from one point to another
         size = len(self.coordinates)
-        edges_length = np.zeros([size, size])  # matrix of edges lengths
+        edges_weight = np.zeros([size, size])  # matrix of edges weights
         for i in range(size):
             for j in range(size):
                 if i == j:  # no distance from vertex to each self
                     pass
-                elif self.adjacency[i, j] == 0:
-                    edges_length[i, j] = edges_length[j, i] = np.Inf
+                elif self.adjacency[i, j] == 0:  # unreachable vertex ~ Inf distance -> Inf weight
+                    edges_weight[i, j] = edges_weight[j, i] = np.Inf
                 else:
                     distance = scale * np.linalg.norm(self.coordinates[j] - self.coordinates[i])
-                    edges_length[i, j] = edges_length[j, i] = distance
-        return edges_length
+                    edges_weight[i, j] = edges_weight[j, i] = distance
+        return edges_weight
+
+    def get_short_route(self):
+        edges_length = self.get_edges_weight()  # get matrix of edges weight
+        size = len(edges_length)
+        shortest_length = np.Inf * np.ones([size, size])  # i,j - shortest length to i vertex with no more than j edges
+        shortest_length[0] = size * [0]  # shortest path from 1 to 1 vertices is 0
+        # implementation of Bellman-Ford algorithm
+        for k in range(0, size - 1):
+            for m in range(0, size):
+                for n in range(0, size):
+                    if shortest_length[m, k] + edges_length[m, n] < shortest_length[n][k + 1:size]:
+                        shortest_length[n, k + 1:size] = shortest_length[m, k] + edges_length[m, n]
+        return shortest_length
 
 
 def adj_matrix_comp(vertex_vector):  # computation of adjacency matrix with vector of vertex coordinates as input
@@ -41,7 +54,7 @@ def adj_matrix_comp(vertex_vector):  # computation of adjacency matrix with vect
     for i in range(size):
         for j in range(size):
             if i != j:  # avoid same vertices
-                distance = np.linalg.norm(vertex_vector[i]-vertex_vector[j]) # distance between two vertices
+                distance = np.linalg.norm(vertex_vector[i]-vertex_vector[j])  # distance between two vertices
                 if distance * scale < speed * battery_life:  # if vertex is reachable
                     adj_matrix[i, j] = adj_matrix[j, i] = 1
     return adj_matrix
