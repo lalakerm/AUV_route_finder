18ce6640850c3e2e1f8157d2b755c61f4241ef63
pavmin <eminpav@gmail.com>
2019-01-20 22:47:24 +0300

    implemented side get_times() function in NMEA

--- NMEA.py
+++ NMEA.py
@@ -1,9 +1,9 @@
 # Represent NMEA message as an object
 import numpy as np
+import datetime
 
 
 class NMEA:
-
     NMEA_template = '$UTHDG,{time},{dist},T,{angle},{is_end}'
 
     def __init__(self, time, dist, turn, angle):
@@ -31,5 +31,17 @@ def get_angles(coordinates, route=()):
     return angles
 
 
-def get_times(edge_weight, route, charging_time, speed):
-    pass
+def get_times(start_time, edges_weight, speed, route, charging_time):  # start_time format HH.MM.SS
+    times = list()
+    time = datetime.time(*list(map(int, start_time.split('.'))))
+    time_matrix = np.copy(edges_weight)
+    for i in range(0, len(time_matrix)):
+        for j in range(0, len(time_matrix)):
+            time_matrix[i, j] /= (speed / 60)  # 60 to get minutes in time_matrix
+    for i in range(len(route) - 1, 0, -1):
+        add_minutes = time_matrix[route[i] - 1, route[i - 1] - 1]  # minutes to add to current time
+        time = (datetime.datetime.combine(datetime.date(1, 1, 1), time)
+                + datetime.timedelta(
+                    minutes=add_minutes + charging_time)).time()  # time of arrival+charging at the vertex
+        times.append(time)
+    return times
