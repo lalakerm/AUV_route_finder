e4efc110f9eee5f41d3f9fdf25f08d166df695a3
pavmin <eminpav@gmail.com>
2019-01-24 19:43:44 +0300

    changed /data/input_data.txt structure, add txt_parse.py instead of DataParse

---
+++ txt_parse.py
@@ -0,0 +1,44 @@
+import os
+
+INFO_POINTS = ('start point', 'final point', 'refuel points',
+               'scale(1:km)', 'speed(km/h)', 'battery life(h)',
+               'charging time(min)')
+
+
+def txt_parse(filepath):
+    try:
+        file = open(os.getcwd() + '/data/' + filepath, 'r')
+        text = file.readlines()
+        if check_structure(text, INFO_POINTS):
+            data = dict()
+            for p in INFO_POINTS:
+                for line in text:
+                    if p in line:
+                        data[p] = get_value(line, p)
+        else:
+            print('Check file structure.')
+    except FileNotFoundError:
+        print('File not found, check directory.')
+    return data
+
+
+def check_structure(text, info_points):  # check file structure
+    for p in info_points:
+        i = 0  # number of info_point occurrence in file
+        for line in text:
+            if p in line:
+                i += 1
+        if i != 1:
+            return False
+    return True
+
+def get_value(line, p):
+    if p in INFO_POINTS[0:3]:
+        return eval(line[line.find('('):line.rfind(')')+1])
+    else:
+        return [int(s) for s in line.split() if s.isdigit()][0]
+
+
+if __name__ == '__main__':
+    print(txt_parse('input_data.txt'))
+
