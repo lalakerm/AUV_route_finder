a7a704b7d94957f9aa8a8e8c426f3a08a31de119
pavmin <eminpav@gmail.com>
2019-02-14 20:10:11 +0300

    add properties to Route class and Vehicle class

--- NMEA/Route.py
+++ NMEA/Route.py
@@ -1,6 +1,56 @@
-# Class represent route with start,end points and refuel_points
+"""
+
+This module provide Route class to represent route model
+with start, end points and possible refill points
+
+"""
+
+
 class Route:
-    def __init__(self, start, end, refill=()):
-        self.start = start
-        self.end = end
-        self.refill = refill
+    def __init__(self, start=(), end=(), refill=()):
+        if len(start) != 2 or len(end) != 2:
+            raise ValueError("Start and End should represent 2D-coordinate(ex. (3,2))")
+        for e in refill:
+            if len(e) != 2:
+                raise ValueError("Refill should represent 2D-coordinate(ex. ((3,2),(2,3)) "
+                                 "or a sequence of coordinates or empty sequence")
+        self._start = start
+        self._end = end
+        self._refill = refill
+
+    @property
+    def start(self):
+        return self._start
+
+    @start.setter
+    def start(self, value):
+        if len(value) == 2:
+            self._start = value
+        else:
+            raise ValueError("Start should represent 2D-coordinate(ex. (3,2))")
+
+    @property
+    def end(self):
+        return self._end
+
+    @end.setter
+    def end(self, value):
+        if len(value) == 2:
+            self._end = value
+        else:
+            raise ValueError("End should represent 2D-coordinate(ex. (3,2))")
+
+    @property
+    def refill(self):
+        return self._refill
+
+    @refill.setter
+    def refill(self, value):
+        if value == ():
+            self._refill = value
+        else:
+            for e in value:
+                if len(e) != 2:
+                    raise ValueError("Refill should represent 2D-coordinate(ex. ((3,2),(2,3)) "
+                                     "or a sequence of coordinates or empty sequence")
+            self._refill = value
--- NMEA/Vehicle.py
+++ NMEA/Vehicle.py
@@ -1,6 +1,49 @@
-# Class represent moving battery-vehicle with average speed, battery life and charging time
+"""
+
+This module provide Vehicle class to represent vehicle model
+with speed, battery_life and charging time
+
+"""
+
+
 class Vehicle:
-    def __init__(self, speed,  battery_life, charging_time):
-        self.speed = speed
-        self.battery_life = battery_life
-        self.charging_time = charging_time
+    def __init__(self, speed,  battery_life, charging_time=0):
+        if charging_time < 0 or speed <= 0 or battery_life <= 0:
+            raise ValueError("Values should be positive "
+                             "(charging time should be 0 or positive)")
+        self._speed = speed
+        self._battery_life = battery_life
+        self._charging_time = charging_time
+
+    @property
+    def speed(self):
+        return self._speed
+
+    @speed.setter
+    def speed(self, value):
+        if value > 0:
+            self._speed = value
+        else:
+            raise ValueError("Vehicle speed must be positive")
+
+    @property
+    def battery_life(self):
+        return self._battery_life
+
+    @battery_life.setter
+    def battery_life(self, value):
+        if value > 0:
+            self._battery_life = value
+        else:
+            raise ValueError("Battery life speed must be positive")
+
+    @property
+    def charging_time(self):
+        return self._charging_time
+
+    @charging_time.setter
+    def charging_time(self, value):
+        if value >= 0:
+            self._charging_time = value
+        else:
+            raise ValueError("Charging time speed must be positive or zero")
